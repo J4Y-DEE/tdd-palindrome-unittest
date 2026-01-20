604893801201a1a2251ed1c5fd51aadcc65465fb
J4YDEE <hollidrap@gmail.com>
2026-01-20 23:54:01 +0000

    red phase

---
+++ test_palindrome.py
@@ -0,0 +1,9 @@
+import unittest
+from main import is_palindrome
+
+class TestPalidrome(unittest.TestCase):
+    def test_simple_palindrome(self):
+        self.assertTrue(is_palindrome("racecar"))
+
+if __name__ == "__main__":
+    unittest.main()
