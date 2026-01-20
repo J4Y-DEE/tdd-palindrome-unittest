905bfddb34a60b03058a33e21142028213910a3c
J4YDEE <hollidrap@gmail.com>
2026-01-20 23:57:02 +0000

    red phase 3 spaces

--- test_palindrome.py
+++ test_palindrome.py
@@ -3,7 +3,7 @@ from main import is_palindrome
 
 class TestPalidrome(unittest.TestCase):
     def test_simple_palindrome(self):
-        self.assertTrue(is_palindrome("RaceCar"))
+        self.assertTrue(is_palindrome("nurses run"))
 
 if __name__ == "__main__":
     unittest.main()
