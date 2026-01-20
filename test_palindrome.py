91bff7aa090c4df97d4fbbd6f8a91344edfcfccc
J4YDEE <hollidrap@gmail.com>
2026-01-20 23:55:50 +0000

    Red 2 mixed string

--- test_palindrome.py
+++ test_palindrome.py
@@ -3,7 +3,7 @@ from main import is_palindrome
 
 class TestPalidrome(unittest.TestCase):
     def test_simple_palindrome(self):
-        self.assertTrue(is_palindrome("racecar"))
+        self.assertTrue(is_palindrome("RaceCar"))
 
 if __name__ == "__main__":
     unittest.main()
